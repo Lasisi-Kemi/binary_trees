10b2a422a3bac3b8852f98aea032d1272c33e97b
Lasisi-Kemi <opeyemioluwakemi6@gmail.com>
2022-09-22 12:43:07 -0700

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,16 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes an entire binary tree
- * @tree: pointer to the root node of the tree to delete
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: a pointer to the node to insert the right-child in
+ * @value: value is the value to store in the new node
  *
- * Return: nothing
+ * Return: a pointer to the created node, or NULL on failure
  */
-void binary_tree_delete(binary_tree_t *tree)
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-if (tree == NULL)
-return;
-binary_tree_delete(tree->left);
-binary_tree_delete(tree->right);
-free(tree);
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
 }
